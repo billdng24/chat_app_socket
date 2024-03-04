3723d9f1571c9d4ed76ea9bfe4630c77dca7b104
billdng24 <nguyenkudo8436@gmail.com>
2024-03-04 15:18:04 +0700

    commit init

---
+++ socket.js
@@ -0,0 +1,48 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT;
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+// on: Dùng để lắng nghe sự kiện của công mà socket.io đang nghe
+// emit: Dùng để bắn đi một sự kiện (Sự kiện được gửi từ on)
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+
+  // Lắng nghe sự kiện gửi tin nhắn
+  socket.on("sendMessage", (response) => {
+    const { UserSendId, UserReceiverId, Content } = response;
+
+    const newResponse = {  UserSendId, UserReceiverId, Content }
+    // Đẩy ra ngoài thông qua emit
+    io.emit("getMessage", newResponse);
+  });
+
+  socket.on("sendRequest", (response) => {
+    const { UserSendId, UserReceiverId } = response;
+
+    const newResponse = { UserSendId, UserReceiverId };
+    io.emit("getRequest", newResponse);
+  })
+
+  socket.on("deleteRequest", (response) => {
+    const {} = response;
+    const newResponse = {};
+    io.emit("getRequest", newResponse);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected")
+  })
+});
+
+// Lắng nghe cổng
+io.listen(port);
